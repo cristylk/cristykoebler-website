68619ea44986b7bdb4c22d253ab92c0b7838ab66
cristylk <cristy.koebler@gmail.com>
2014-08-07 14:50:03 -0400

    Updated index and style pages

---
+++ script.js
@@ -0,0 +1,55 @@
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+<script src="http://cristykoebler.com/jquery_plugins/stickUp-master/stickUp.min.js"></script>
+
+<script type="text/javascript"> 
+	//initiating jQuery 
+	/*	jQuery(function($) { 
+			$(document).ready( function() { //enabling stickUp on the '.navbar-wrapper' class 
+				$('.navigation').stickUp(); 
+			}); 
+		}); */
+	</script>
+
+	<script>
+	jQuery(document).ready(function($){
+	// browser window scroll (in pixels) after which the "back to top" link is shown
+	var offset = 300,
+		//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+		offset_opacity = 1200,
+		//duration of the top scrolling animation (in ms)
+		scroll_top_duration = 700,
+		//grab the "back to top" link
+		$back_to_top = $('.cd-top');
+
+	//hide or show the "back to top" link
+	$(window).scroll(function(){
+		( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
+		if( $(this).scrollTop() > offset_opacity ) { 
+			$back_to_top.addClass('cd-fade-out');
+		}
+	});
+
+	//smooth scroll to top
+	$back_to_top.on('click', function(event){
+		event.preventDefault();
+		$('body,html').animate({
+			scrollTop: 0 ,
+		 	}, scroll_top_duration
+		);
+	});
+
+	});
+	</script>
+
+	<script>
+  		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  		ga('create', 'UA-26194880-1', 'auto');
+  		ga('send', 'pageview');
+
+	</script>
+ 
